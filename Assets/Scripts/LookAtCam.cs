0ff17dc6106cb87e756054f7c1c2d868489663d1
Stephen Grosjean <stephen-gm@hotmail.com>
2019-06-13 20:46:18 +0200

    Added dynamic Camera

---
+++ Assets/Scripts/LookAtCam.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCam : MonoBehaviour
+{
+    [SerializeField] private Transform firstTarget, secondTarget;
+    [SerializeField] private float xOffset, fovOffset;
+    public Vector3 middleVector;
+    public float point;
+    public float distance;
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(middleVector, 0.5f);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector3.Distance(firstTarget.position, secondTarget.position);
+
+        middleVector = distance/2 * Vector3.Normalize(secondTarget.position - firstTarget.position) + firstTarget.position;
+        Camera.main.gameObject.transform.position = new Vector3(middleVector.x+xOffset, Camera.main.gameObject.transform.position.y, middleVector.z);
+        if (distance + fovOffset > 65) {
+            Camera.main.fieldOfView = distance + fovOffset;
+        }
+    }
+}
