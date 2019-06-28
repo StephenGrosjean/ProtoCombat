b43a8d246de03e239eef25834e27876ddc0b3af6
Stephen Grosjean <stephen-gm@hotmail.com>
2019-06-28 15:40:13 +0200

    Added room join

---
+++ Assets/Scripts/Networking/ButtonJoinRoom.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class ButtonJoinRoom : MonoBehaviour
+{
+    public void JoinRoom() {
+        PhotonNetwork.JoinRoom(gameObject.name);
+    }
+}
