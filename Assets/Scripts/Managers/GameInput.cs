cd579f8f9367c78af13b13644add89254dc29ee2
Duncan Bourquard <bourquardduncan@gmail.com>
2019-08-09 11:01:15 +0200

    Bumper and trigger will be recognized as controller input

--- Assets/Scripts/Managers/GameInput.cs
+++ Assets/Scripts/Managers/GameInput.cs
@@ -307,30 +307,17 @@ public static class GameInput {
     private static void CheckControllerLastUsed()
     {
         InputDevice device = InputManager.ActiveDevice;
-        if (Input.anyKey || Input.GetMouseButton(0) || Input.GetMouseButton(1))
+        if (Input.anyKeyDown || Input.GetMouseButton(0) || Input.GetMouseButton(1))
         {
             keyboardLastPressed = true;
         }
-        if (Input.GetKey(KeyCode.Joystick1Button0) ||
-             Input.GetKey(KeyCode.Joystick1Button1) ||
-             Input.GetKey(KeyCode.Joystick1Button2) ||
-             Input.GetKey(KeyCode.Joystick1Button3) ||
-             Input.GetKey(KeyCode.Joystick1Button4) ||
-             Input.GetKey(KeyCode.Joystick1Button5) ||
-             Input.GetKey(KeyCode.Joystick1Button6) ||
-             Input.GetKey(KeyCode.Joystick1Button7) ||
-             Input.GetKey(KeyCode.Joystick1Button8) ||
-             Input.GetKey(KeyCode.Joystick1Button9) ||
-             Input.GetKey(KeyCode.Joystick1Button10) ||
-             Input.GetKey(KeyCode.Joystick1Button11) ||
-             Input.GetKey(KeyCode.Joystick1Button12) ||
-             Input.GetKey(KeyCode.Joystick1Button13) ||
-             Input.GetKey(KeyCode.Joystick1Button14) ||
-             Input.GetKey(KeyCode.Joystick1Button15) ||
-             Input.GetKey(KeyCode.Joystick1Button16) ||
-             Input.GetKey(KeyCode.Joystick1Button17) ||
-             Input.GetKey(KeyCode.Joystick1Button18) ||
-             Input.GetKey(KeyCode.Joystick1Button19))
+        if (device.AnyButtonWasPressed || 
+            device.AnyButtonIsPressed || 
+            device.AnyButtonWasReleased ||
+            device.LeftBumper.IsPressed ||
+            device.RightBumper.IsPressed ||
+            device.LeftTrigger.IsPressed ||
+            device.RightTrigger.IsPressed)
         {
             keyboardLastPressed = false;
         }
