add93c8ed368331eaf3c9fab1143710c1ab87c7c
Stephen Grosjean <stephen-gm@hotmail.com>
2019-08-14 11:27:11 +0200

    Added new arena

--- Assets/Scripts/LookAtCam.cs
+++ Assets/Scripts/LookAtCam.cs
@@ -4,21 +4,14 @@ using UnityEngine;
 using Photon.Realtime;
 using Photon.Pun;
 using UnityEngine.SceneManagement;
+using Cinemachine;
 /// <summary>
 /// Script to make the camera look at two points
 /// </summary>
 public class LookAtCam : MonoBehaviour
 {
     [SerializeField] private Transform firstTarget, secondTarget; //Targets to look at
-    [SerializeField] private float xOffset, fovOffset; //Offsets
-    private Vector3 middleVector = Vector3.zero; //Vector between two target
-    private float distance; //Distance between two target
-    
-    //Draw middle point
-    private void OnDrawGizmosSelected() {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(middleVector, 0.5f);
-    }
+    [SerializeField] private CinemachineTargetGroup targetGroup;
 
     private void Start() {
         Invoke("FindTargets", 1);
@@ -65,17 +58,14 @@ public class LookAtCam : MonoBehaviour
     void Update()
     {
         if (firstTarget != null && secondTarget != null) {
-            distance = Vector3.Distance(firstTarget.position, secondTarget.position); //Find distance between the two targets
-
-            middleVector = distance / 2 * Vector3.Normalize(secondTarget.position - firstTarget.position) + firstTarget.position; //Find the middle vector between the two targets
-            transform.position = new Vector3(middleVector.x + xOffset, transform.position.y, middleVector.z); //Set the camera position to the middle vector position plus the offset
-            float fovCalc = 0.7f * (distance + fovOffset) + 21.8f; //Define the fov 
-            if (fovCalc > 65) { //Set the fov of the camera 
-                Camera.main.fieldOfView = fovCalc;
-            }
+            targetGroup.m_Targets[0].target = firstTarget;
+            targetGroup.m_Targets[1].target = secondTarget;  
         }
         else {
             FindTargets();
         }
     }
+
+
+   
 }
