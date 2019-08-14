bc26e59d4ae245c4d6f7d2ee182226b2c9f8b677
Stephen Grosjean <stephen-gm@hotmail.com>
2019-08-14 20:25:01 +0200

    Added names in waitroom

---
+++ Assets/Scripts/Menu/WaitRoomNames.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class WaitRoomNames : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI masterName, clientName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        masterName.text = PhotonNetwork.MasterClient.NickName;
+
+        if (PhotonNetwork.IsMasterClient) {
+            Player[] players = PhotonNetwork.PlayerListOthers;
+            if (players.Length > 0) {
+                clientName.text = players[0].NickName;
+            }
+            else {
+                clientName.text = "[Waiting]";
+            }
+        }
+        else {
+            clientName.text = PhotonNetwork.NickName;
+        }
+    }
+}
